d366922a44ba0108bb1dd93e2b1bd872e01f4da6
Admin <admin@AdmindeMacBook-Pro-91.local>
2018-08-26 15:01:33 +0800

    modi

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 """
 Retrain the YOLO model for your own dataset.
 """
+import os
+import sys
+import gflags
 
 import numpy as np
 import keras.backend as K
@@ -13,11 +16,30 @@ from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_l
 from yolo3.utils import get_random_data
 
 
-def _main():
-    annotation_path = '2007_train.txt'
-    log_dir = 'logs/000/'
-    classes_path = 'model_data/voc_classes.txt'
-    anchors_path = 'model_data/yolo_anchors.txt'
+gflags.DEFINE_string(
+        "annotation_path",
+        "2007_train.txt",
+        "The path of the annotation file")
+gflags.DEFINE_string(
+        "log_dir",
+        "logs/000/",
+        "The dir of the log file")
+gflags.DEFINE_string(
+        "model_data",
+        "model_data",
+        "The dir of the model file")
+gflags.DEFINE_string(
+        "pretrained_model_path",
+        "model_data/tiny_yolo_weights.h5",
+        "The path of the pretrained model")
+FLAGS = gflags.FLAGS
+
+def main(argv):
+    FLAGS(argv)
+    annotation_path = FLAGS.annotation_path
+    log_dir = FLAGS.log_dir
+    classes_path = os.path.join(FLAGS.model_data, 'voc_classes.txt')
+    anchors_path = os.path.join(FLAGS.model_data, 'yolo_anchors.txt')
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
@@ -26,17 +48,39 @@ def _main():
 
     is_tiny_version = len(anchors)==6 # default setting
     if is_tiny_version:
-        model = create_tiny_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
+        model = create_tiny_model(
+                input_shape,
+                anchors,
+                num_classes,
+                freeze_body=2,
+                weights_path='model_data/tiny_yolo_weights.h5')
     else:
-        model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+        model = create_model(
+                input_shape,
+                anchors,
+                num_classes,
+                freeze_body=2,
+                weights_path=FLAGS.pretrained_model_path)
 
     logging = TensorBoard(log_dir=log_dir)
-    checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
-        monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
-    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
+    checkpoint = ModelCheckpoint(
+            log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
+            monitor='val_loss',
+            save_weights_only=True,
+            save_best_only=True,
+            period=1)
+
+    reduce_lr = ReduceLROnPlateau(
+            monitor='val_loss',
+            factor=0.1,
+            patience=3,
+            verbose=1)
+
+    early_stopping = EarlyStopping(
+            monitor='val_loss',
+            min_delta=0,
+            patience=10,
+            verbose=1)
 
     val_split = 0.1
     with open(annotation_path) as f:
@@ -48,21 +92,37 @@ def _main():
     num_train = len(lines) - num_val
 
     # Train with frozen layers first, to get a stable loss.
-    # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
+    # Adjust num epochs to your dataset.
+    # This step is enough to obtain a not bad model.
     if True:
-        model.compile(optimizer=Adam(lr=1e-3), loss={
-            # use custom yolo_loss Lambda layer.
-            'yolo_loss': lambda y_true, y_pred: y_pred})
+        model.compile(
+                optimizer=Adam(lr=1e-3),
+                loss={'yolo_loss': lambda y_true, y_pred: y_pred})
 
         batch_size = 32
-        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+        print('Train on {} samples, '
+              'val on {} samples, '
+              'with batch size {}.'.format(num_train, num_val, batch_size))
+
+        model.fit_generator(
+                data_generator_wrapper(
+                        lines[:num_train],
+                        batch_size,
+                        input_shape,
+                        anchors,
+                        num_classes),
                 steps_per_epoch=max(1, num_train//batch_size),
-                validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
+                validation_data=data_generator_wrapper(
+                        lines[num_train:],
+                        batch_size,
+                        input_shape,
+                        anchors,
+                        num_classes),
                 validation_steps=max(1, num_val//batch_size),
-                epochs=50,
-                initial_epoch=0,
+                epochs=22,
+                initial_epoch=21,
                 callbacks=[logging, checkpoint])
+
         model.save_weights(log_dir + 'trained_weights_stage_1.h5')
 
     # Unfreeze and continue training, to fine-tune.
@@ -70,41 +130,79 @@ def _main():
     if True:
         for i in range(len(model.layers)):
             model.layers[i].trainable = True
-        model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
-        batch_size = 32 # note that more GPU memory is required after unfreezing the body
-        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
-            steps_per_epoch=max(1, num_train//batch_size),
-            validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
-            validation_steps=max(1, num_val//batch_size),
-            epochs=100,
-            initial_epoch=50,
-            callbacks=[logging, checkpoint, reduce_lr, early_stopping])
+        model.compile(
+                optimizer=Adam(lr=1e-4),
+                loss={'yolo_loss': lambda y_true, y_pred: y_pred})
+
+        batch_size = 32
+        print('Train on {} samples,'
+              'val on {} samples,'
+              'with batch size {}.'.format(num_train, num_val, batch_size))
+
+        model.fit_generator(
+                data_generator_wrapper(
+                        lines[:num_train],
+                        batch_size,
+                        input_shape,
+                        anchors,
+                        num_classes),
+                steps_per_epoch=max(1, num_train//batch_size),
+                validation_data=data_generator_wrapper(
+                        lines[num_train:],
+                        batch_size,
+                        input_shape,
+                        anchors,
+                        num_classes),
+                validation_steps=max(1, num_val//batch_size),
+                epochs=100,
+                initial_epoch=50,
+                callbacks=[logging, checkpoint, reduce_lr, early_stopping])
+
         model.save_weights(log_dir + 'trained_weights_final.h5')
 
     # Further training if needed.
 
 
 def get_classes(classes_path):
-    '''loads the classes'''
+    """
+    load the classes
+    :param classes_path:
+    :return:
+    """
     with open(classes_path) as f:
         class_names = f.readlines()
     class_names = [c.strip() for c in class_names]
     return class_names
 
 def get_anchors(anchors_path):
-    '''loads the anchors from a file'''
+    """
+    load the anchors
+    """
     with open(anchors_path) as f:
         anchors = f.readline()
     anchors = [float(x) for x in anchors.split(',')]
     return np.array(anchors).reshape(-1, 2)
 
 
-def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze_body=2,
-            weights_path='model_data/yolo_weights.h5'):
-    '''create the training model'''
+def create_model(input_shape,
+                 anchors,
+                 num_classes,
+                 load_pretrained=True,
+                 freeze_body=2,
+                 weights_path='model_data/yolo_weights.h5'):
+    """
+    create the training model, set the trainable part of the model
+    :param input_shape:
+    :param anchors:
+    :param num_classes:
+    :param load_pretrained:
+    :param freeze_body:
+    :param weights_path:
+    :return:
+    """
+
     K.clear_session() # get a new session
     image_input = Input(shape=(None, None, 3))
     h, w = input_shape
@@ -120,7 +218,7 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
         model_body.load_weights(weights_path, by_name=True, skip_mismatch=True)
         print('Load weights {}.'.format(weights_path))
         if freeze_body in [1, 2]:
-            # Freeze darknet53 body or freeze all but 3 output layers.
+            # Freeze the model  body or freeze all but 3 output layers.
             num = (185, len(model_body.layers)-3)[freeze_body-1]
             for i in range(num): model_body.layers[i].trainable = False
             print('Freeze the first {} layers of total {} layers.'.format(num, len(model_body.layers)))
@@ -134,7 +232,16 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
 
 def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=True, freeze_body=2,
             weights_path='model_data/tiny_yolo_weights.h5'):
-    '''create the training model, for Tiny YOLOv3'''
+    """
+    create the training model, for the Tiny YOLOv3
+    :param input_shape:
+    :param anchors:
+    :param num_classes:
+    :param load_pretrained:
+    :param freeze_body:
+    :param weights_path:
+    :return:
+    """
     K.clear_session() # get a new session
     image_input = Input(shape=(None, None, 3))
     h, w = input_shape
@@ -163,7 +270,15 @@ def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=True, f
     return model
 
 def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes):
-    '''data generator for fit_generator'''
+    """
+    data generator for fit_generator
+    :param annotation_lines:
+    :param batch_size:
+    :param input_shape:
+    :param anchors:
+    :param num_classes:
+    :return:
+    """
     n = len(annotation_lines)
     i = 0
     while True:
@@ -187,4 +302,4 @@ def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, n
     return data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes)
 
 if __name__ == '__main__':
-    _main()
+    main(sys.argv)
