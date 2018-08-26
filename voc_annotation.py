839ce0b78e83b47d68940689c2b48ac77326a9c2
Admin <admin@AdmindeMacBook-Pro-91.local>
2018-08-26 19:25:59 +0800

    modi

--- voc_annotation.py
+++ voc_annotation.py
@@ -1,18 +1,19 @@
 import xml.etree.ElementTree as ET
 from os import getcwd
 
-sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+sets=[('GS', 'train'), ('GS', 'val'), ('GS', 'test')]
 
 classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car",
            "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike",
            "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
 
-def convert_annotation(year, image_id, list_file):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+def convert_annotation(appdix, image_id):
+    in_file = open('VOCdevkit/VOC_%s/Annotations/%s.xml'%(appdix, image_id))
     tree=ET.parse(in_file)
     root = tree.getroot()
 
+    line = ""
     for obj in root.iter('object'):
         difficult = obj.find('difficult').text
         cls = obj.find('name').text
@@ -20,17 +21,26 @@ def convert_annotation(year, image_id, list_file):
             continue
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
-        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
-        list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
+        b = (int(xmlbox.find('xmin').text),
+             int(xmlbox.find('ymin').text),
+             int(xmlbox.find('xmax').text),
+             int(xmlbox.find('ymax').text))
+        line += (" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
+    return line
 
 wd = getcwd()
 
-for year, image_set in sets:
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
-    list_file = open('%s_%s.txt'%(year, image_set), 'w')
+for appdix, image_set in sets:
+    image_ids = open(
+            'VOCdevkit/VOC_%s/ImageSets/Main/%s.txt'%(appdix, image_set)
+    ).read().strip().split()
+
+    list_file = open('%s_%s.txt'%(appdix, image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
-        convert_annotation(year, image_id, list_file)
+        list_file.write(
+                '%s/VOCdevkit/VOC_%s/JPEGImages/%s.jpg'%(wd, appdix, image_id))
+
+        list_file.write(convert_annotation(appdix, image_id))
         list_file.write('\n')
     list_file.close()
 
