2094efe4e19e3647ff6e44ce9dfcf67e47843c91
Admin <admin@AdmindeMacBook-Pro-91.local>
2018-09-09 01:31:28 +0800

    modi

--- yolo.py
+++ yolo.py
@@ -4,14 +4,13 @@ Class definition of YOLO_v3 style detection model on image and video
 """
 
 import colorsys
-import os
 from timeit import default_timer as timer
 
 import numpy as np
 from keras import backend as K
 from keras.models import load_model
 from keras.layers import Input
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image
 
 from yolo3.model import yolo_eval, yolo_body, tiny_yolo_body
 from yolo3.utils import letterbox_image
@@ -20,7 +19,7 @@ from keras.utils import multi_gpu_model
 
 class YOLO(object):
     _defaults = {
-        "model_path": 'model_data/trained_weights_final.h5',
+        "model_path": 'logs/20180901/ep070-loss11.862-val_loss13.069.h5',
         "anchors_path": 'model_data/anchors.txt',
         "classes_path": 'model_data/classes.txt',
         "score" : 0.5,
@@ -100,8 +99,8 @@ class YOLO(object):
                 score_threshold=self.score, iou_threshold=self.iou)
         return boxes, scores, classes
 
-    def detect_image(self, image):
-        start = timer()
+    def detect_image(self, image, img, output_folder, class_set):
+        # start = timer()
 
         if self.model_image_size != (None, None):
             assert self.model_image_size[0]%32 == 0, 'Multiples of 32 required'
@@ -113,7 +112,7 @@ class YOLO(object):
             boxed_image = letterbox_image(image, new_image_size)
         image_data = np.array(boxed_image, dtype='float32')
 
-        print(image_data.shape)
+        # print(image_data.shape)
         image_data /= 255.
         image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
 
@@ -125,46 +124,36 @@ class YOLO(object):
                 K.learning_phase(): 0
             })
 
-        print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
+        # print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
 
-        font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
-                    size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
-        thickness = (image.size[0] + image.size[1]) // 300
+        if len(list(enumerate(out_classes))) == 0:
+            image.save(output_folder + '/' + img)
+        else:
+            count = 0
+            for i, c in reversed(list(enumerate(out_classes))):
+                predicted_class = self.class_names[c]
+                if count >= 1:
+                    continue
+                if predicted_class not in class_set:
+                    continue
+                count += 1
+                box = out_boxes[i]
+                score = out_scores[i]
 
-        for i, c in reversed(list(enumerate(out_classes))):
-            predicted_class = self.class_names[c]
-            box = out_boxes[i]
-            score = out_scores[i]
+                # label = '{} {:.2f}'.format(predicted_class, score)
 
-            label = '{} {:.2f}'.format(predicted_class, score)
-            draw = ImageDraw.Draw(image)
-            label_size = draw.textsize(label, font)
+                top, left, bottom, right = box
+                top = max(0, np.floor(top + 0.5).astype('int32'))
+                left = max(0, np.floor(left + 0.5).astype('int32'))
+                bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+                right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+                image.crop([left, top, right, bottom]).save(
+                        output_folder+'/'+img.split(".")[0]+'_'+predicted_class+'.jpg')
+            if count == 0:
+                image.save(output_folder + '/' + img)
 
-            top, left, bottom, right = box
-            top = max(0, np.floor(top + 0.5).astype('int32'))
-            left = max(0, np.floor(left + 0.5).astype('int32'))
-            bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
-            right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
-            print(label, (left, top), (right, bottom))
-
-            if top - label_size[1] >= 0:
-                text_origin = np.array([left, top - label_size[1]])
-            else:
-                text_origin = np.array([left, top + 1])
-
-            # My kingdom for a good redistributable image drawing library.
-            for i in range(thickness):
-                draw.rectangle(
-                    [left + i, top + i, right - i, bottom - i],
-                    outline=self.colors[c])
-            draw.rectangle(
-                [tuple(text_origin), tuple(text_origin + label_size)],
-                fill=self.colors[c])
-            draw.text(text_origin, label, fill=(0, 0, 0), font=font)
-            del draw
-
-        end = timer()
-        print(end - start)
+        # end = timer()
+        # print(end - start)
         return image
 
     def close_session(self):
--- yolo_inference.py
+++ yolo_inference.py
@@ -6,22 +6,70 @@ import os
 from yolo import YOLO
 from PIL import Image
 
-# import gflags
+from timeit import default_timer as timer
 
-input_folder = "/home/wutenghu/git_wutenghu/gs_images/B43170101"
+img_root_folder = "gs_img"
+dcd_folder = "B43"
+
+new_dcd_classes_dict = {
+    'B43130517': {'sweater', 'blouse', 'shirt', 't_shirt', 'polo_shirt'},  # t shirt
+    'B43130701': {'dress', 'skirt', 'pants'},  # pants
+    'B43050103': {},                           # bra/panty set
+    'B43130509': {'coat', 'sweater', 'blouse', 'shirt', 't_shirt', 'polo_shirt'},    # blouse
+    'B43130301': {'dress', 'skirt', 'pants'},  # dress
+    'B43050107': {},  # panties
+    'B43050501': {},  # socks
+    'B43070903': {},  # swimsuit
+    'B43130511': {'coat', 'sweater', 'blouse', 'shirt', 't_shirt',
+                  'polo_shirt'},  # jacket
+    'B43130501': {'sweater', 'blouse', 'shirt', 't_shirt', 'polo_shirt'},  # shirt
+    'B43130519': {'coat', 'sweater', 'blouse', 'shirt', 't_shirt',
+                  'polo_shirt'},  # cardigan
+    'B43130503': {'coat', 'sweater', 'blouse', 'shirt', 't_shirt',
+                  'polo_shirt'},  # knit/sweater
+    'B43130703': {'dress', 'skirt', 'pants'}  # skirt
+}
 
 def detect_img(yolo):
 
-    img_list = os.listdir(input_folder)
-    for img in img_list:
-        img_path = os.path.join(input_folder, img)
-        image = Image.open(img_path)
-        r_image = yolo.detect_image(image)
-        r_image.save(input_folder+'/'+img.split('.')[0]+'_.jpg')
-    yolo.close_session()
+    cwd = os.getcwd()
+    new_dcd_path = os.path.join(cwd, img_root_folder, dcd_folder)
+    crop_new_dcd_path = new_dcd_path + "_crop"
+
+    if not os.path.exists(crop_new_dcd_path):
+        os.mkdir(crop_new_dcd_path)
+
+    new_dcd_list = os.listdir(new_dcd_path)
+
+    for new_dcd in new_dcd_list:
+        print(new_dcd)
+        start = timer()
+
+        img_path = os.path.join(new_dcd_path, new_dcd)
+        img_list = os.listdir(img_path)
+        class_set = new_dcd_classes_dict[new_dcd]
+
+        crop_img_path = os.path.join(crop_new_dcd_path, new_dcd)
+        if not os.path.exists(crop_img_path):
+            os.mkdir(crop_img_path)
+
+        count = 0
+        for img in img_list:
+            if count % 1000 == 0:
+                print("{} progressed {:.4f} %".format(new_dcd, count*100/len(img_list)))
+            try:
+                image = Image.open(img_path+'/'+img)
+                r_image = yolo.detect_image(image, img, crop_img_path, class_set)
+            except Exception as e:
+                print(img_path)
+            count += 1
+
+        end = timer()
+        print("Total cost {} seconds.".format(end - start))
+        yolo.close_session()
 
 def main():
-    detect_img(YOLO())
+    detect_img(yolo=YOLO())
 
 if __name__ == '__main__':
     main()
